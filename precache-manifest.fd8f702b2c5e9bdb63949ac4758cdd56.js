3fd309bbd200d71068bf6ad645aec30b3d56a2f8
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-06-06 23:41:14 -0400

    Updates

---
+++ precache-manifest.fd8f702b2c5e9bdb63949ac4758cdd56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb028410498d3dbf5074572417cb2222",
+    "url": "/mamtasonar1127/to-do-app/index.html"
+  },
+  {
+    "revision": "b9695bf25eb767b4404d",
+    "url": "/mamtasonar1127/to-do-app/static/css/main.45b08bb0.chunk.css"
+  },
+  {
+    "revision": "71b51a011a013f00dea5",
+    "url": "/mamtasonar1127/to-do-app/static/js/2.e212fbf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mamtasonar1127/to-do-app/static/js/2.e212fbf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9695bf25eb767b4404d",
+    "url": "/mamtasonar1127/to-do-app/static/js/main.ee180891.chunk.js"
+  },
+  {
+    "revision": "05c6875dc00ff2ccabd9",
+    "url": "/mamtasonar1127/to-do-app/static/js/runtime-main.ff495288.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mamtasonar1127/to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
